420a0c082b4ce5535158509b72bf15a257ed8437
alekhyabol <alekhya.batchu@lji.io>
2024-07-11 10:48:07 +0530

    Initial Commit

---
+++ Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by Batchu Lakshmi Alekhya on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
